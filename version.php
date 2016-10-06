dc1377af227e4da53d5a942a49f66985fe0289ff
Daniel Neis Araujo <danielneis@gmail.com>
2016-10-06 17:21:19 -0300

    bump version

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2016033000;
+$plugin->version = 2016100600;
 $plugin->requires = 2015050500;
-$plugin->release = 3;
+$plugin->release = 4;
 $plugin->maturity   = MATURITY_STABLE;
 $plugin->component = 'availability_paypal';
